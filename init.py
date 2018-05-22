bb8a568ec0a148e1ee77bfee633bdf1b9a4eefa6
kalom_ti@hotmail.com <abner@swapnil.localdomain>
2018-05-22 19:42:00 +0800

    current state

--- init.py
+++ init.py
@@ -1,55 +1,59 @@
-import scipy as *
+import numpy as np
 
-size([random(size) for index in range(3)]) = 1
 
 # write a function to return neighbours
-def main():
+def main(size):
 
     #currently starts out with empty grid
     #in the future if random positions are populated it will be included
     class model:
-
         def __init__(self,size):
-            self.space = array([size for i in range(3)])
+            self.space = np.zeros([size for i in range(3)])
 
-
+        @staticmethod
         def populate_nbs(position):
-            neighbour =  [[position[0]+1,position[1],position[2]],[position[0]-1,position[1],position[2]],[position[0],position[1]+1,position[2]],[position[0],position[1]-1,position[2]],[position[0],position[1],position[2]+1],[position[0],position[1],position[2]-1]]
-            for index,item in enumerate(position):
-                if item == 0:
+            neighbour =  ([[position[0]+1,position[1],position[2]],
+                           [position[0]-1,position[1],position[2]],
+                           [position[0],position[1]+1,position[2]],
+                           [position[0],position[1]-1,position[2]],
+                           [position[0],position[1],position[2]+1],
+                           [position[0],position[1],position[2]-1]])
 
-
-
-            @classmethod
-        def vaccant_nbs(self,position):
+        @classmethod
+        def vaccant_nbs(self, position):
             # position is a list of length 3
             # this function should only return empty neighbours
             vaccant_deems = []
-            for i in populate_nbs(position):
-                if self.space[i] == 1:
-                vaccant_deems.append(self.space[i])
-        return vaccant_deems
+            for locations in self.populate_nbs(position):
+                for checks in locations:
+                    if checks > size:
+                        break
+                if self.space[locations] == 0:
+                    vaccant_deems.append(self.space[locations])
+            return vaccant_deems
 
-    grid = model(100)
+        @classmethod
+        def grid_chooser(self,position):
+            vaccant_sites = self.vaccant_nbs(position)
+            return vaccant_sites[np.random.randint(len(vaccant_sites))]
 
+        def poisson_migration(self,position):
+            choice = self.grid_chooser(position)
+            if self.space[choice] == 0:
+                self.space[choice] = 1
+                print(self.space)
+            else:
+                poisson_migration(choice)
 
+    grid = model(size)
+    position  = [np.random.randint(size) for i in range(3)]
+    #here is the issue!
+    grid.space[position] = 1
+    print(grid.space)
 
+    grid.poisson_migration(position)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__=='__main__':
+    main(5)
 
 
